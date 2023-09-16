f9e060bd8127c18a06f49672a53cb7387050edbd
lawalhabib <130799966+lawalhabib@users.noreply.github.com>
2023-09-16 17:03:16 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// function sayHi() {
+//     console.log("Hiiiiii");
+// }
+
+// sayHi();
+
+// function multiply(a, b) {
+//      return a * b
+// }
+
+// const sum = multiply(2, 6);
+// console.log(sum);
+
+// function sing() {
+//     console.log("Top of the world");
+//     console.log("Flyyyyyyyy");
+// }
+
+// sing();
+
+function sing(song) {
+    console.log(song);
+}
+
+
+sing("Welcome back");
+sing("Welcome front");
+
+
+
+
